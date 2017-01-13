20c43b7458f0666d272e475e4cfa00b21b407600
StephanK9 <stephane.khoury@gmail.com>
2017-01-13 10:52:20 -0800

    add model data and display of model data on index page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,24 @@
 import Ember from 'ember';
 
+var questions = [{
+  id: 1,
+  author: "Veruca Salt",
+  content: "What are bees?",
+  notes: "I need this question answered fast!"
+}, {
+  id: 2,
+  author: "Mike TV",
+  content: "What are elephants?",
+  notes: "I need this question answered now!"
+}, {
+  id: 3,
+  author: "Violet Beauregarde",
+  content: "When does the eclipse happen?",
+  notes: "I love space stuff!"
+}];
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  },
 });
